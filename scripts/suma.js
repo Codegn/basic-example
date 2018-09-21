3cb21d648a189340c4ff03245a747288c878f8bf
Nicolas Villalobos Zaid <nvillalobos@sectra.cl>
2018-09-21 10:38:36 -0300

    first commit

---
+++ scripts/suma.js
@@ -0,0 +1,9 @@
+  function myFunction(arg1, arg2, arg3) {
+      var x, y,sum;
+      x = parseFloat(arg1);
+      y = parseFloat(arg2);
+      sum = x + y;
+      arg3 = sum;
+      alert('Hello, ' + parseString(sum) + '!');
+//      return sum;
+  }
